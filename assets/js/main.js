89f901744a2b71b00dbde9bf1a1bd754e3def9e9
emanuele <emattina97@gmail.com>
2021-03-26 15:56:35 +0100

    Carico topbar sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+// milestone 1
+    // 1 - replica deòòa grafoca con possibilità di avere messaggi scritti dall'utente (verdi) e dall'interlocutore (bianchi) assegnando due classi css diverse
+    // 2 - visualizzazione dimanica delal lista contatti tramite v-for usando nome e immagine di ogni contatto
+    
 var app = new Vue ({
     el: '#root',
     data: {
