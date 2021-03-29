319352c07eab4a87350ea6956190418d4b854119
emanuele <emattina97@gmail.com>
2021-03-29 16:20:04 +0200

    Carico milestone 3 e 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,18 +8,15 @@ var app = new Vue ({
         contacts: contacts,
         contactActive: 0,
         inputMessage: '',
+        inputSearch: '',
     },
     computed: {
-        // lastMsg: function () {    
-        //     this.contacts.forEach((messaggio, index) => {
-        //         this.contacts[index].lastMessage = this.contacts[index].messages[this.contacts[index].messages.length - 1].text;
-
-        //         console.log(messaggio);
-        //         console.log(index);
-        //         console.log(this.lastMessage);
-        //     });
-        // }
-    },
+        searchList() {
+            return this.contacts.filter(contatto => {
+                return contatto.name.toLowerCase().includes(this.inputSearch.toLowerCase())
+            });
+        }
+      },
     methods: {
         dateMessage: function(date) {
             let data = new Date(date);
@@ -30,15 +27,23 @@ var app = new Vue ({
         setActive: function(index) {
             this.contactActive = index;
         },
-        sendMessage: function() {
+        sendMessage: function(contactActive) {
             if(this.inputMessage != '') {
                 let newMessage = {
                     date: '10/01/2020 15:30:55',
                     text: this.inputMessage,
                     status: 'sent'
                 }
-            this.contacts[index].messages.push(newMessage);
-            this.inputMessage = '';
+                this.contacts[contactActive].messages.push(newMessage);
+                this.inputMessage = '';
+                setTimeout(function() {
+                    let newMessage = {
+                        date: '10/01/2020 15:30:55',
+                        text: 'ok',
+                        status: 'received'
+                    }
+                this.contacts[contactActive].messages.push(newMessage);
+                }, 1000);
             }
         },
     }
